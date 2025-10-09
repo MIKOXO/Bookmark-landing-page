aa239cabeac83467c5b7bbe944715f60da7cf762
MIKOXO <mikermount19@yahoo.com>
2025-10-09 17:02:21 +0300

    Minor edit made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
           {/* Mobile menu overlay */}
           <div
             className={clsx(
-              "fixed top-0 left-0 w-full h-full pt-10 opacity-95 bg-Blue950 text-white flex flex-col items-center space-y-8 uppercase tracking-widest transition-transform duration-300 ease-in-out",
+              "z-50 fixed top-0 left-0 w-full h-full pt-10 opacity-95 bg-Blue950 text-white flex flex-col items-center space-y-8 uppercase tracking-widest transition-transform duration-300 ease-in-out",
               isOpen ? "translate-x-0" : "translate-x-full"
             )}
           >
